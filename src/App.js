687028e1ec368258237cbd5cb8f965653414ce58
GodwinEgo <godwinehile04@gmail.com>
2023-02-17 17:21:21 +0100

    App

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () =>
       <Box sx={ { backgroundColor: '#000' } }>
         <Navbar />
         <Routes>
-          <Route path='/feed' exact element={ <Feed /> } />
+          <Route path='/' exact element={ <Feed /> } />
           <Route path='/video/:id' element={ <VideoDetail /> } />
           <Route path='/channel/:id' element={ <ChannelDetail /> } />
           <Route path='/search/:id' element={ <SearchFeed /> } />
