32479a833ffb90102b5e170ea9b8149f9b5817e3
GodwinEgo <godwinehile04@gmail.com>
2023-02-17 17:18:53 +0100

    Nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     p={ 2 }
     sx={ { position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between' } }
   >
-    <Link to="/" style={{display:'flex', alignItems:'center'}}>
+    <Link to="/" style={{display: 'flex', alignItems: 'center'}}>
       <img src={logo} alt='logo' height={45}/>
     </Link>
  </Stack>
