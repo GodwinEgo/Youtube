0937eaee65ee529b9150b412d2515c54fb86081c
GodwinEgo <godwinehile04@gmail.com>
2023-02-19 00:59:00 +0100

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,23 @@
 import { Stack } from '@mui/system'
-import { Button } from 'bootstrap'
 import React from 'react'
 
 import { categories } from '../utils/constants'
+
+const selectedCategory = 'New';
 
 
 const Sidebar = () =>
 {
   return (
-    <Stack direction="row" sx={ { overflowY: 'auto', height: { sx: 'auto', md: '95%' }, flexDirection: { md: 'column' } } }>
+    <Stack direction="row" sx={ { overflowY: 'auto', height: { sx: 'auto', md: '95%' }, flexDirection: { md: 'column' }, } }>
       { categories.map( ( category ) => (
-        <Button>
+        <button
+          className='category-btn'
+          style={{background: category.name === selectedCategory && '#FC1503', color: '#fff'}}
+        >
           <span>{ category.icon }</span>
           <span>{ category.name}</span>
-        </Button>
+        </button>
       ) ) }
     </Stack>
   )
