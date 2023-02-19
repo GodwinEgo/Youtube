ec681597d765ce07114d9fc67943a0ae22df8366
GodwinEgo <godwinehile04@gmail.com>
2023-02-19 01:26:07 +0100

    Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { Box, Typography,  Stack } from '@mui/material'
-import Sidebar from './Sidebar'
 
+import { Sidebar, Videos} from './'
 
 
 const Feed = () => {
@@ -18,14 +18,19 @@ const Feed = () => {
         </Typography>
       </Box>
 
+      
+
       <Box p={2} sx={{overflowY:'auto', height: '90vh', flex:2}}>
         <Typography variant='h4' fontWeight="bold" mb={ 2 } sx={{color: 'white'}}>
           New <span style={{color: '#F31503'}}>
             Videos
           </span>
         </Typography>
-        <Videos/>
+        <Videos  videos={[]} />
       </Box>
+
+
+      
    </Stack>
   )
 }
