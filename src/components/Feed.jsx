d15b1d26b0403e6d4e9432fa4c2aa5da526286d7
GodwinEgo <godwinehile04@gmail.com>
2023-02-19 13:56:22 +0100

    Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -21,7 +21,10 @@ const Feed = () =>
     <Stack sx={{flexDirection:{sx:"column", md: 'row'}}}>
       <Box sx={ { height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px:{sx: 0, md:2}}}>
      
-        <Sidebar />
+        <Sidebar
+          selectedCategory={ selectedCategory }
+          setSelectedCategory={ setSelectedCategory }
+        />
         
 
         <Typography className='copyright' variant='body2' sx={{mt:1.5,color:'#fff'}}>
@@ -33,7 +36,7 @@ const Feed = () =>
 
       <Box p={2} sx={{overflowY:'auto', height: '90vh', flex:2}}>
         <Typography variant='h4' fontWeight="bold" mb={ 2 } sx={{color: 'white'}}>
-          New <span style={{color: '#F31503'}}>
+          {selectedCategory} <span style={{color: '#F31503'}}>
             Videos
           </span>
         </Typography>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 
 import { categories } from '../utils/constants'
 
-const selectedCategory = 'New';
 
 
-const Sidebar = () =>
+const Sidebar = ({selectedCategory, setSelectedCategory}) =>
 {
   return (
     <Stack direction="row" sx={ { overflowY: 'auto', height: { sx: 'auto', md: '95%' }, flexDirection: { md: 'column' }, } }>
       { categories.map( ( category ) => (
         <button
           className='category-btn'
+          onClick={ () => setSelectedCategory(category.name)}
           style={ { background: category.name === selectedCategory && '#FC1503', color: '#fff' } }
           key={category.name}
         >
