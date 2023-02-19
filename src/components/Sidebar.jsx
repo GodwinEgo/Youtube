dd5af2a11598ca4e3aab9bcd5f056a5b0df68dbb
GodwinEgo <godwinehile04@gmail.com>
2023-02-19 01:15:11 +0100

    Navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,10 +13,11 @@ const Sidebar = () =>
       { categories.map( ( category ) => (
         <button
           className='category-btn'
-          style={{background: category.name === selectedCategory && '#FC1503', color: '#fff'}}
+          style={ { background: category.name === selectedCategory && '#FC1503', color: '#fff' } }
+          key={category.name}
         >
-          <span>{ category.icon }</span>
-          <span>{ category.name}</span>
+          <span style={{color:  category.name === selectedCategory ? 'white': 'red', marginRight: '15px'}}>{ category.icon }</span>
+          <span style={ { opacity: category.name === selectedCategory ? '1' : '0.8'}}>{ category.name}</span>
         </button>
       ) ) }
     </Stack>
