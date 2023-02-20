cd9d2587a949bdf5f5b25635d9019b8568ff8149
GodwinEgo <godwinehile04@gmail.com>
2023-02-20 18:38:38 +0100

    VideoCard

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -18,7 +18,19 @@ const VideoCard = ( { video: { id: { videoId }, snippet } } ) =>
           sx={ { width: 358, height: 180 } }
         />
       </Link>
-      <CardContent sx={ { backgroundColor: '#1e1e1e', height: '106px' } } />
+      <CardContent sx={ { backgroundColor: '#1e1e1e', height: '106px' } }>
+        <Link to={ videoId ? `/video/${ videoId }` : demoVideoUrl }>
+          <Typography variant='subtitle1' fontWeight="bold" color="#fff">
+            { snippet?.title.slice( 0, 60 ) || demoVideoTitle.slice( 0, 60 ) }
+          </Typography>
+        </Link>
+        <Link to={ snippet?.chanelId ? `/channel/${ snippet?.chanelId }` : demoChannelUrl }>
+          <Typography variant='subtitle2' fontWeight="bold" color="gray">
+            { snippet?.channelTitle.slice( 0, 60 ) || demoChannelTitle.slice( 0, 60 ) }
+            <CheckCircle sx={ { fontSize: 12, color: "gray", ml: '5px' } } />
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   )
 }
