e09105478bba54cc108d4d117f0b5c0dbe019832
GodwinEgo <godwinehile04@gmail.com>
2023-02-20 18:26:51 +0100

    VideoCard

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -3,21 +3,22 @@ import { Link } from 'react-router-dom'
 import { Typography, Card, CardContent, CardMedia } from '@mui/material'
 import { CheckCircle } from '@mui/icons-material'
 
-import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl,demoChannelTitle } from '../utils/constants'
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants'
 
 
 
-const VideoCard = ({video: {id:{videoId}, snippet}}) =>
+const VideoCard = ( { video: { id: { videoId }, snippet } } ) =>
 {
   return (
     <Card>
-      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+      <Link to={ videoId ? `/video/${ videoId }` : demoVideoUrl }>
         <CardMedia
-          image={ snippet?.thumbnails?.url }
+          image={ snippet?.thumbnails?.high?.url }
           alt={ snippet?.title }
-          sx={{width:358, height: 180}}
+          sx={ { width: 358, height: 180 } }
         />
       </Link>
+      <CardContent sx={ { backgroundColor: '#1e1e1e', height: '106px' } } />
     </Card>
   )
 }
