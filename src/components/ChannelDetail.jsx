8f4f835ed214cc368ed56549822fef013e19ad66
GodwinEgo <godwinehile04@gmail.com>
2023-02-24 00:25:24 +0100

    Channeldetail

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,13 +1,13 @@
 import { React, useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Box } from '@mui/system'
-
+import { Videos, ChannelCard } from './'
 
 const ChannelDetail = () =>
 {
   return (
     <div>
-      ChannelDetail
+
     </div>
   )
 }
